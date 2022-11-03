8b7f47eb6507af0113da15724437cbf1efc93930
Kaike silva <107559196+devkaike@users.noreply.github.com>
2022-11-03 20:19:41 -0300

    Update LeituraArq.java

--- LeituraArq.java
+++ LeituraArq.java
@@ -7,16 +7,15 @@ import java.io.IOException;
 public class LeituraArq {
 	public static void main(String[] args) {
 		try {
-			ManipulacaoDeString x = new ManipulacaoDeString();
+			
 			String f = "";
 			BufferedReader in = new BufferedReader(new FileReader("/home/puc/eclipse-workspace/aaaaaa/Atividade"));
 			String frase = "";
-			int i = 0;
 			while(in.ready()) {
+				ManipulacaoDeString x = new ManipulacaoDeString();
 				frase = in.readLine();
-				f += frase +"\n\r";
-				x.separaFrase(f, i);
-				i++;
+				//f += frase +"\n\r";
+				x.separaFrase(frase);
 				System.out.print(x.getSequencia()+" - "+x.getNome()+" - "+x.getSalario() +" - "+ x.getGratificacao()+"\n"+ x.getMostraDep());
 			}
 			//System.out.print(x.getNome());
