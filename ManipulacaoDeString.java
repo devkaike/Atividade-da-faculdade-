f98dc54874391294d6f4af5bc9b4bdda9087346c
Kaike silva <107559196+devkaike@users.noreply.github.com>
2022-11-03 20:17:29 -0300

    Update ManipulacaoDeString.java

--- ManipulacaoDeString.java
+++ ManipulacaoDeString.java
@@ -28,29 +28,37 @@ public class ManipulacaoDeString {
 		this.nome = nome;
 	}
 
-	public void separaFrase(String frase, int qtd) {
-		String[] nSeparado = frase.split("\n");//separa por quebra de linha
-		String[] x = nSeparado[0].split("-");//separa por -
-		String sal = x[qtd+2];
-		String grat = x[qtd+3];
-		nome = x[qtd+1];
-		sequencia = x[qtd];
+	public void separaFrase(String frase) {
+		//String[] nSeparado = frase.split("\n");//separa por quebra de linha
+		String[] x = frase.split("-");//separa por -
+		sequencia = x[0];
+		nome = x[1];
+		String sal = x[2];
+		String grat = x[3];
+		
+		
 		mGratificacao(grat);
-		mSalario(sal);	
-		if(x.length > 4) {
-			mostraDep = "";
-			nomeDep = x[qtd+4];
-			String data = x[qtd+5];
-			manipulaData(data);
-			if(x[6].charAt(0) == 'M') {
-				sexoDep = "MASCULINO";
-			}else {
-				sexoDep = "FEMININO";
+		mSalario(sal);
+		int i = 0;
+		i = x.length - 4;
+		mostraDep = "";
+		if(i != 0) {
+			for(int j = 0; j < i; j = j + 3) {
+				
+				nomeDep = x[j+4];
+				String data = x[j+5];
+				manipulaData(data);
+				if(x[j+6].charAt(0) == 'M') {
+					sexoDep = "MASCULINO";
+				}else {
+					sexoDep = "FEMININO";
+				}
+				mostraDep += "     "+nomeDep +" - "+ dataDep +" - "+ sexoDep+"\n";
 			}
-			mostraDep = "     "+nomeDep +" - "+ dataDep +" - "+ sexoDep;
+			
 		}else {
 			
-			mostraDep = "                <Sem filhos>";
+			mostraDep = "                <Sem filhos>\n";
 		}
 	}
 
@@ -156,6 +164,14 @@ public class ManipulacaoDeString {
 			aux += grat.charAt(1);
 			aux += grat.charAt(2);
 		}
+		if(grat.length() == 2) {
+			aux += grat.charAt(0);
+			aux += ",";
+			aux += grat.charAt(1);
+		}
+		if(grat.length() == 1) {
+			aux += grat.charAt(0);
+		}
 		gratificacao = aux;
 		return gratificacao;
 	}
@@ -170,54 +186,65 @@ public class ManipulacaoDeString {
 			a++;
 		}
 		//if(sal.charAt(0) == '0') {
-			for(int i = a; i < sal.length(); i ++) {
+			
+		//}
+		for(int i = a; i < sal.length(); i++) {
 				aux1 += sal.charAt(i);
 			}
 			sal = aux1;
-		//}
-
-		if(sal.length() == 7) {
-			aux += sal.charAt(0);
-			aux += sal.charAt(1);
-			aux += ".";
-			aux += sal.charAt(2);
-			aux += sal.charAt(3);
-			aux += sal.charAt(4);
-			aux += ",";
-			aux += sal.charAt(5);
-			aux += sal.charAt(6);
+		if(a == sal.length()) {
+			aux += "0,0";
+			
+		}else {
+			if(sal.length() == 7) {
+				aux += sal.charAt(0);
+				aux += sal.charAt(1);
+				aux += ".";
+				aux += sal.charAt(2);
+				aux += sal.charAt(3);
+				aux += sal.charAt(4);
+				aux += ",";
+				aux += sal.charAt(5);
+				aux += sal.charAt(6);
+			}
+			if(sal.length() == 6) {
+				aux += sal.charAt(0);
+				aux += ".";
+				aux += sal.charAt(1);
+				aux += sal.charAt(2);
+				aux += sal.charAt(3);
+				aux += ",";
+				aux += sal.charAt(4);
+				aux += sal.charAt(5);
+			}
+			if(sal.length() == 5) {
+				aux += sal.charAt(0);
+				aux += sal.charAt(1);
+				aux += sal.charAt(2);
+				aux += ",";
+				aux += sal.charAt(3);
+				aux += sal.charAt(4);
+			}
+			if(sal.length() == 4) {
+				aux += sal.charAt(0);
+				aux += sal.charAt(1);
+				aux += ",";
+				aux += sal.charAt(2);
+				aux += sal.charAt(3);
+			}
+			if(sal.length() == 3) {
+				aux += sal.charAt(0);
+				aux += ",";
+				aux += sal.charAt(1);
+				aux += sal.charAt(2);
+			}
+			if(sal.length() == 2) {
+				aux += sal.charAt(0);
+				aux += ",";
+				aux += sal.charAt(1);
+			}
 		}
-		if(sal.length() == 6) {
-			aux += sal.charAt(0);
-			aux += ".";
-			aux += sal.charAt(1);
-			aux += sal.charAt(2);
-			aux += sal.charAt(3);
-			aux += ",";
-			aux += sal.charAt(4);
-			aux += sal.charAt(5);
-		}
-		if(sal.length() == 5) {
-			aux += sal.charAt(0);
-			aux += sal.charAt(1);
-			aux += sal.charAt(2);
-			aux += ",";
-			aux += sal.charAt(3);
-			aux += sal.charAt(4);
-		}
-		if(sal.length() == 4) {
-			aux += sal.charAt(0);
-			aux += sal.charAt(1);
-			aux += ",";
-			aux += sal.charAt(2);
-			aux += sal.charAt(3);
-		}
-		if(sal.length() == 3) {
-			aux += sal.charAt(0);
-			aux += ",";
-			aux += sal.charAt(1);
-			aux += sal.charAt(2);
-		}
+		
 		salario = aux;
 		return salario;
 	}
