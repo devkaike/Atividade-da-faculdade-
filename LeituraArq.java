f10e1d4a32f4614a096f2601585dcd83e015ee4a
Kaike silva <107559196+devkaike@users.noreply.github.com>
2022-11-03 15:06:36 -0300

    Update LeituraArq.java

--- LeituraArq.java
+++ LeituraArq.java
@@ -1,3 +1,5 @@
+package aaaaaa;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -7,7 +9,7 @@ public class LeituraArq {
 		try {
 			ManipulacaoDeString x = new ManipulacaoDeString();
 			String f = "";
-			BufferedReader in = new BufferedReader(new FileReader("/home/puc/eclipse-workspace/Att/Atividade"));
+			BufferedReader in = new BufferedReader(new FileReader("/home/puc/eclipse-workspace/aaaaaa/Atividade"));
 			String frase = "";
 			int i = 0;
 			while(in.ready()) {
@@ -15,9 +17,10 @@ public class LeituraArq {
 				f += frase +"\n\r";
 				x.separaFrase(f, i);
 				i++;
-				System.out.print(x.getSequencia()+" - "+x.getNome()+" - "+x.getSalario() +" - "+ x.getGratificacao()+"\n");
+				System.out.print(x.getSequencia()+" - "+x.getNome()+" - "+x.getSalario() +" - "+ x.getGratificacao()+"\n"+ x.getMostraDep());
 			}
-			
+			//System.out.print(x.getNome());
+
 		}catch(IOException e) {
 			System.out.print("ERRO!");
 		}
